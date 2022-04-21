37a970fba67cf61b462b55cffb05977162e32c24
naz <inaztm@gmail.com>
2022-04-21 20:05:36 +0300

    add more story

--- src/components/tiptap-editor.stories.ts
+++ src/components/tiptap-editor.stories.ts
@@ -11,12 +11,23 @@ export default {
   argTypes: {
     onChange: { action: 'changed' },
   },
-  render: (args) => html`<tiptap-editor @onChange=${args.onChange} .value=${args.value}></tiptap-editor>`,
+  render: (args) => html`
+  <div style="min-width: 500px">
+    <tiptap-editor @onChange=${args.onChange} .value=${args.value}></tiptap-editor>
+  </div>
+  `,
 } as Meta
 
 export const Default: StoryObj = {
   name: 'Default',
   args: {
     value: 'Lit',
+  },
+}
+
+export const Link: StoryObj = {
+  name: 'Link',
+  args: {
+    value: '<p>Test text editor, link&nbsp;<a target="_blank" rel="noopener noreferrer nofollow" class="link" href="https://youtu.be/lfjxMhvCNmA">https://youtu.be/lfjxMhvCNmA</a></p>',
   },
 }
